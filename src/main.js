87291779256d35e29509755a61bf372c19c83f02
Admin <admin@Mac-Admin.local>
2017-02-28 23:06:57 -0500

    flux

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@
 var React = require('react');
 var ReactRouter = require('react-router');
 var routes = require('./routes');
+var InitializeActions = require('./actions/initializeActions');
+
+InitializeActions.initApp();
 
 ReactRouter.run(routes, function(Handler){
   React.render(<Handler />, document.getElementById('app'));
